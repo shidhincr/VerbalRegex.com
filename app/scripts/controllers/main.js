d3522237b9de6da58346f3b4acf2d2279e7ca750
Shidhin <shidhin.cr@flipkart.com>
2014-01-17 21:30:24 +0530

    setting up the navigation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-angular.module('verbalregexApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module( 'verbalregexApp' )
+  .controller( 'MainCtrl', function( $scope ) {
+    $scope.menuItems = [ 'Home', 'About', 'Contact' ];
+    $scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
+  } );
