52e173d883b14ba54f92c182be60dcb542d1c063
Shidhin <shidhin.cr@flipkart.com>
2014-01-17 19:17:58 +0530

    Initial scaffolding using yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('verbalregexApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
