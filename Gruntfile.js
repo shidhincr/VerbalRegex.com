7c27550121859c6a4809a32f1aa5c217b2256a0c
Shidhin <shidhin.cr@flipkart.com>
2014-01-21 17:19:11 +0530

    Adding the background gradient and grunt task for prefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,43 +7,43 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function (grunt) {
+module.exports = function( grunt ) {
 
   // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+  require( 'load-grunt-tasks' )( grunt );
 
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+  require( 'time-grunt' )( grunt );
 
   // Define the configuration for all the tasks
-  grunt.initConfig({
+  grunt.initConfig( {
 
     // Project settings
     yeoman: {
       // configurable paths
-      app: require('./bower.json').appPath || 'app',
+      app: require( './bower.json' ).appPath || 'app',
       dist: 'dist'
     },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all'],
+        files: [ '<%= yeoman.app %>/scripts/{,*/}*.js' ],
+        tasks: [ 'newer:jshint:all' ],
         options: {
           livereload: true
         }
       },
       jsTest: {
-        files: ['test/spec/{,*/}*.js'],
-        tasks: ['newer:jshint:test', 'karma']
+        files: [ 'test/spec/{,*/}*.js' ],
+        tasks: [ 'newer:jshint:test', 'karma' ]
       },
       compass: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        tasks: ['compass:server', 'autoprefixer']
+        files: [ '<%= yeoman.app %>/styles/{,*/}*.{scss,sass}' ],
+        tasks: [ 'compass:server', 'autoprefixer' ]
       },
       gruntfile: {
-        files: ['Gruntfile.js']
+        files: [ 'Gruntfile.js' ]
       },
       livereload: {
         options: {
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
     jshint: {
       options: {
         jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        reporter: require( 'jshint-stylish' )
       },
       all: [
         'Gruntfile.js',
@@ -105,21 +105,21 @@ module.exports = function (grunt) {
         options: {
           jshintrc: 'test/.jshintrc'
         },
-        src: ['test/spec/{,*/}*.js']
+        src: [ 'test/spec/{,*/}*.js' ]
       }
     },
 
     // Empties folders to start fresh
     clean: {
       dist: {
-        files: [{
+        files: [ {
           dot: true,
           src: [
             '.tmp',
             '<%= yeoman.dist %>/*',
             '!<%= yeoman.dist %>/.git*'
           ]
-        }]
+        } ]
       },
       server: '.tmp'
     },
@@ -127,15 +127,15 @@ module.exports = function (grunt) {
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
-        browsers: ['last 1 version']
+        browsers: [ 'last 2 version' ]
       },
       dist: {
-        files: [{
+        files: [ {
           expand: true,
           cwd: '.tmp/styles/',
           src: '{,*/}*.css',
           dest: '.tmp/styles/'
-        }]
+        } ]
       }
     },
 
@@ -205,32 +205,32 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on rev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
-      css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      html: [ '<%= yeoman.dist %>/{,*/}*.html' ],
+      css: [ '<%= yeoman.dist %>/styles/{,*/}*.css' ],
       options: {
-        assetsDirs: ['<%= yeoman.dist %>']
+        assetsDirs: [ '<%= yeoman.dist %>' ]
       }
     },
 
     // The following *-min tasks produce minified files in the dist folder
     imagemin: {
       dist: {
-        files: [{
+        files: [ {
           expand: true,
           cwd: '<%= yeoman.app %>/images',
           src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/images'
-        }]
+        } ]
       }
     },
     svgmin: {
       dist: {
-        files: [{
+        files: [ {
           expand: true,
           cwd: '<%= yeoman.app %>/images',
           src: '{,*/}*.svg',
           dest: '<%= yeoman.dist %>/images'
-        }]
+        } ]
       }
     },
     htmlmin: {
@@ -241,12 +241,12 @@ module.exports = function (grunt) {
           removeCommentsFromCDATA: true,
           removeOptionalTags: true
         },
-        files: [{
+        files: [ {
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          src: [ '*.html', 'views/{,*/}*.html' ],
           dest: '<%= yeoman.dist %>'
-        }]
+        } ]
       }
     },
 
@@ -254,26 +254,26 @@ module.exports = function (grunt) {
     // minsafe compatible so Uglify does not destroy the ng references
     ngmin: {
       dist: {
-        files: [{
+        files: [ {
           expand: true,
           cwd: '.tmp/concat/scripts',
           src: '*.js',
           dest: '.tmp/concat/scripts'
-        }]
+        } ]
       }
     },
 
     // Replace Google CDN references
     cdnify: {
       dist: {
-        html: ['<%= yeoman.dist %>/*.html']
+        html: [ '<%= yeoman.dist %>/*.html' ]
       }
     },
 
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
-        files: [{
+        files: [ {
           expand: true,
           dot: true,
           cwd: '<%= yeoman.app %>',
@@ -291,8 +291,8 @@ module.exports = function (grunt) {
           expand: true,
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
-          src: ['generated/*']
-        }]
+          src: [ 'generated/*' ]
+        } ]
       },
       styles: {
         expand: true,
@@ -350,38 +350,38 @@ module.exports = function (grunt) {
         singleRun: true
       }
     }
-  });
+  } );
 
 
-  grunt.registerTask('serve', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+  grunt.registerTask( 'serve', function( target ) {
+    if ( target === 'dist' ) {
+      return grunt.task.run( [ 'build', 'connect:dist:keepalive' ] );
     }
 
-    grunt.task.run([
+    grunt.task.run( [
       'clean:server',
       'bower-install',
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
       'watch'
-    ]);
-  });
+    ] );
+  } );
 
-  grunt.registerTask('server', function () {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve']);
-  });
+  grunt.registerTask( 'server', function( ) {
+    grunt.log.warn( 'The `server` task has been deprecated. Use `grunt serve` to start a server.' );
+    grunt.task.run( [ 'serve' ] );
+  } );
 
-  grunt.registerTask('test', [
+  grunt.registerTask( 'test', [
     'clean:server',
     'concurrent:test',
     'autoprefixer',
     'connect:test',
     'karma'
-  ]);
+  ] );
 
-  grunt.registerTask('build', [
+  grunt.registerTask( 'build', [
     'clean:dist',
     'bower-install',
     'useminPrepare',
@@ -396,11 +396,11 @@ module.exports = function (grunt) {
     'rev',
     'usemin',
     'htmlmin'
-  ]);
+  ] );
 
-  grunt.registerTask('default', [
+  grunt.registerTask( 'default', [
     'newer:jshint',
     'test',
     'build'
-  ]);
+  ] );
 };
